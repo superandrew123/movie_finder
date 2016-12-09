3aa00177fdcca17e4726802c168e21cc0c986035
superandrew123 <andrewmiksch@gmail.com>
2016-12-08 23:23:49 -0500

    javascript set-up with unit testing for MovieFinder. Routes and controller next

---
+++ chrome_extension/js/moviefinder.js
@@ -0,0 +1,21 @@
+MovieFinder = {
+  init: function(){
+
+  },
+  readInput: function(keyPress){
+    debugger;
+  },
+  search: function(search_term){
+    $.ajax({
+      method: 'get', 
+      dataType: 'json', 
+      url: 'http://localhost:3000/search',
+      data: {
+        q: search_term
+      },
+      complete: ()=>{
+        debugger;
+      }
+    });
+  }
+}
