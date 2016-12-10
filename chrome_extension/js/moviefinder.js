686f8297651cc88be3b65c3048bd1aaeec2c503b
superandrew123 <andrewmiksch@gmail.com>
2016-12-09 22:53:46 -0500

    animations happening in the right order

--- chrome_extension/js/moviefinder.js
+++ chrome_extension/js/moviefinder.js
@@ -44,23 +44,45 @@ MovieFinder = {
       html += '</div>';
     }
     $('#search-results').html(html);
+    $('#availability-results').html('');
     $('.search-result-container').click(MovieFinder.availabilityRequest);
   },
   availabilityRequest: function(event){
     var movie_id = $(this).attr('data-id');
-    $.ajax({
-      method: 'get',
-      dataType: 'json',
-      url: 'http://localhost:3000/search',
-      data: {
-        movie_id: movie_id
-      },
-      success: function(){
-        debugger;
-      },
-      failure: function(){
-        console.log('availability failure failure');
-      }
+    $('.search-result-container').hide();
+    $(this).show();
+    $("#loading").animate({'height': '160px'},function(){
+      $.ajax({
+        method: 'get',
+        dataType: 'json',
+        url: 'http://localhost:3000/availability',
+        data: {
+          q: movie_id
+        },
+        success: function(data){
+          $("#loading").css({'height': '0px'});
+          MovieFinder.buildAvailabilityResults(data);
+        },
+        failure: function(){
+          console.log('availability failure failure');
+        }
+      });
     });
+  },
+  buildAvailabilityResults: function(data){
+    $("#loading").css({'height': '0px'});
+    if(!!data['error']){
+      let error_html = '<p class="availability-error">Error: ' + data['error'] + '</p>';
+      $('#availability-results').html(error_html)
+      return;
+    }
+    var availability_html = '';
+    availability_html += '<div class="availability-container col-md-6 netflix">';
+      availability_html += '<div><p><span>Neflix:</span> ' + data.netflix + '</p></div>';
+    availability_html += '</div>';
+    availability_html += '<div class="availability-container col-md-6 amazon">';
+      availability_html += '<div><p><span>Amazon:</span> ' + data.amazon + '</p></div>';
+    availability_html += '</div>';
+    $('#availability-results').html(availability_html);
   }
 }
